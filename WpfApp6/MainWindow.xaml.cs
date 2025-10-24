1d4405ec514907fffb4a37393b25ea7102284933
Денис Р <denr60106@gmail.com>
2025-10-24 12:05:21 +0300

    Добавьте файлы проекта.

---
+++ WpfApp6/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfApp6
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnProducts_Click(object sender, RoutedEventArgs e)
+        {
+            //MainFrame.Navigate(new ProductsPage());
+        }
+
+        private void BtnStatistics_Click(object sender, RoutedEventArgs e)
+        {
+           // MainFrame.Navigate(new StatisticsPage());
+        }
+
+        private void BtnSearch_Click(object sender, RoutedEventArgs e)
+        {
+            //MainFrame.Navigate(new SearchPage());
+        }
+    }
+}
